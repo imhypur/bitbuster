0e4274a7c4f0c32b8a0d34321932363abce813c8
imhypur <44926761+imhypur@users.noreply.github.com>
2018-11-10 11:06:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var config = {
   // - Your app's id on moneypot.com
   app_id: 18,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: 'Untitled Dice',
+  app_name: 'BitBister Dice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
@@ -102,9 +102,9 @@ helpers.calcNumber = function(cond, winProb) {
   console.assert(typeof winProb === 'number');
 
   if (cond === '<') {
-    return winProb * 100;
+    return winProb * 10;
   } else {
-    return 99.99 - (winProb * 100);
+    return 50.99 - (winProb * 10);
   }
 };
 
